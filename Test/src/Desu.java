f10eda924b1e1048f3cf303ce9c0a07c2331e541
K <kmcintyremiller@yahoo.com>
2017-09-16 13:22:23 -0500

    first commit

---
+++ Test/src/Desu.java
@@ -0,0 +1,10 @@
+
+public class Desu {
+    public static void main(String[] args) {
+    	System.out.println("Hello World");
+    	
+    	//primitive data type of java.
+        //byte, int, char, long, double, short, float, bool
+    	
+    }
+}
